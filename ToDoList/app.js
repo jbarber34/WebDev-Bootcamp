072fbbc676010951fd0d10c597cba8cde15861ce
James Barber <jamesbarber@twinsbaseball.com>
2022-04-29 15:42:07 -0400

    Updates to ToDoList to incorporate database usage

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,10 +1,18 @@
 // Require necessary packages
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const _ = require("lodash");
 const dates = require(__dirname + '/date.js'); // Custom module
+const config = require('./config.json');
 
 // Create app constant
 const app = express();
+
+// Set the port for the server
+const port = 3000;
+// Find date by using custom function for list titles
+const day = dates.getDate();
 
 // Set app to use ejs as view engine
 app.set('view engine', 'ejs');
@@ -16,76 +24,69 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// Create empty arrays for item to be used later
-// const works here because we're only adding/removing from array, not reassigning
-const items = [];
-const workItems = [];
+// Connect to new database in mongodb
+mongoose.connect(config.MONGOOSE_CONNECT)
 
-// Set the port for the server
-const port = 3000;
+// Create mongoose schema and model
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
 
-// Find date by using custom function for list titles
-const day = dates.getDate();
+const Item = mongoose.model("Item", itemsSchema);
+
+// Create new item for database
+const item1 = new Item({
+  name: "Welcome to your To-Do List!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Create new schema for custom lists
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 // Home page
-app.get("/", function (req, res) {
-  // Render the list.ejs file from views folder
-  // Pass in JS object that has a key value pair
-  res.render("list", {
-    date: day,
-    listTitle: "Personal",
-    newListItems: items
-  })
+app.get("/", (req, res) => {
 
+  // Check what we have in our db
+  Item.find({}, (err, foundItems) => {
+    // Check to see if the database is empty, if so, insert default items
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.")
+        }
+      });
+
+      // Redirect back to the root so added items appear
+      res.redirect("/");
+
+    } else if (err) {
+      console.log(err);
+    } else {
+
+      // Render the list.ejs file from views folder
+      // Pass in JS object that has a key value pair
+      res.render("list", {
+        listTitle: day,
+        newListItems: foundItems
+      });
+    };
+  });
 });
 
-// Get information from HTML Add List Item
-app.post("/", function (req, res) {
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    // If item matches clear, clear the entire list
-    if (item === 'clear') {
-      item = [];
-      // If item matches remove and a number, remove that list item
-    } else if (item == item.match(/remove [0-9]/)) {
-      // Grab the number only to select the item to remove
-      let number = parseInt(item.match(/\d+/)[0], 10);
-      // Remove the desired item from the array
-      workItems == workItems.splice(number - 1, 1);
-    } else {
-      // If not a clear or remove, add the item to the list
-      workItems.push(item);
-    }
-    // Push the response back to the work page to be added to list
-    res.redirect("/work");
-  } else {
-    // If item matches clear, clear the entire list
-    if (item === 'clear') {
-      items = [];
-      // If item matches remove and a number, remove that list item
-    } else if (item == item.match(/remove [0-9]/)) {
-      // Grab the number only to select the item to remove
-      let number = parseInt(item.match(/\d+/)[0], 10);
-      // Remove the desired item from the array
-      items == items.splice(number - 1, 1);
-    } else {
-      // If not a clear or remove, add the item to the list
-      items.push(item);
-    }
-    // Push the response back to the root to be added to list
-    res.redirect("/");
-  }
-});
-
-// Add another route to work page
-app.get("/work", function (req, res) {
-  res.render("list", {
-    date: day,
-    listTitle: "Work",
-    newListItems: workItems
-  })
-})
 
 // Add about page route
 app.get("/about", function (req, res) {
@@ -93,8 +94,100 @@ app.get("/about", function (req, res) {
 })
 
 
+
+// Dynamic routing
+app.get("/:customListName", (req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({ name: customListName }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list if it's not yet created
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+
+      } else {
+        // Show list if it already exists
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
+  });
+});
+
+
+// Get information from HTML Add List Item
+app.post("/", (req, res) => {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === day) {
+    // Redirect to root if item being added to personal list
+    item.save();
+    res.redirect("/");
+  } else {
+    // Else redirect to the custom list
+    List.findOne({ name: listName }, (err, foundList) => {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+
+// Create the post request to delete items from list
+app.post("/delete", (req, res) => {
+
+  // Find which item on the list was checked.
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === day) {
+    // If on the root page:
+    // Remove the checked item from the list after a 1-second delay
+    setTimeout(() => {
+
+      Item.findByIdAndRemove(checkedItemId, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully removed item ID " + checkedItemId + ".")
+          res.redirect("/");
+        }
+      });
+
+    }, 1000);
+  } else {
+    // If coming from custom page
+    setTimeout(() => {
+
+      List.findOneAndUpdate(
+        { name: listName },
+        { $pull: { items: { _id: checkedItemId } } },
+        (err, foundList) => {
+          if (!err) {
+            res.redirect("/" + listName);
+          }
+        });
+
+    }, 1000);
+  }
+});
+
+
 // Make the app listen on the selected port
 // Using || (OR) allows the app to listen on cloud server and local 3000
-app.listen(port || 3000, function () {
+app.listen(port || 3000, () => {
   console.log(`Server is running on port ${port || 3000}`);
 });
