a8d7e56558b1c27a0565c30e5d5105e6b554a077
James Barber <jamesbarber@twinsbaseball.com>
2022-05-08 14:47:04 -0400

    Adding level 1 authentication

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -15,6 +16,19 @@ app.use(express.static("public"));
 
 // Create the port for express to run on
 const port = 3000;
+
+// Connect to mongoose
+mongoose.connect("mongodb://localhost:27017/secretsUserDB")
+
+// Create mongoose schema
+const userSchema = {
+    email: String,
+    password: String
+}
+
+// Build mongoose model
+const User = new mongoose.model("User", userSchema);
+
 
 // Home Page
 app.get("/", (req, res) => {
@@ -31,7 +45,40 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+// Post routes
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({ email: username }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                } else {
+                    res.send("Incorrect Username or Password")
+                };
+            };
+        };
+    });
+});
 
 
 
