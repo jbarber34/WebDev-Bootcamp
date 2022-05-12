cbe7341612b2e0ccd1826019fb45f34e3ffbdf76
James Barber <jamesbarber@twinsbaseball.com>
2022-05-12 17:20:13 -0400

    Adding Level 6 authentication Part 1

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
@@ -52,6 +53,19 @@ passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// Google Authentication Strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+    (accessToken, refreshToken, profile, cb) => {
+        User.findOrCreate({ googleId: profile.id }, (err, user) => {
+            return cb(err, user);
+        });
+    }
+));
 
 
 // Home Page
