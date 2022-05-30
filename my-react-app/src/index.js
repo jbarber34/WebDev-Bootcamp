fcb99afeba993fead851841ba50647d2b716321d
James Barber <jamesbarber@twinsbaseball.com>
2022-05-29 20:11:47 -0400

    Initial push of first React app

---
+++ my-react-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <h1>Hello World!</h1>
+);
+
+// Saving this because course method is outdated and I need to replicate this 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
