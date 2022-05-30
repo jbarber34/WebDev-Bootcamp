be202e1d810eb97e51dfe922d8cfbc817d186bce
James Barber <jamesbarber@twinsbaseball.com>
2022-05-30 12:57:26 -0400

    Adding main components of Keeper App

--- Keeper App/src/index.js
+++ Keeper App/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <h1>Hello World!</h1>
+  <App />
 );
 
 // Saving this because course method is outdated and I need to replicate this 
